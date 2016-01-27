744907d984ff00196f0e0573f1cfc5acf0fef401
Peter Sung <petersung21@gmail.com>
2016-01-26 22:37:31 -0500

    lab1 commit

--- lab1/Test_deque_as_array.cpp
+++ lab1/Test_deque_as_array.cpp
@@ -1,8 +1,7 @@
 //
 //  Test_deque_as_array.cpp
 //  ece250
-//  studentID: 20576785
-//  uwID: phsung
+//
 //  Created by Peter Hyungrok Sung on 2016-01-25.
 //  Copyright (c) 2016 Peter Hyungrok Sung. All rights reserved.
 //
@@ -14,14 +13,38 @@
 int main() {
     Deque_as_array *testDeque = new Deque_as_array(10);
     
-    testDeque->enqueue_head(33);
-    testDeque->enqueue_head(53);
-    testDeque->enqueue_tail(73);
-    testDeque->enqueue_tail(83);
+    try {
+        testDeque->dequeue_tail();
+    } catch (underflow ex) {
+        std::cout << "Proper underflow error thrown" << std::endl;
+    }
+    
+    try {
+        testDeque->dequeue_head();
+    } catch (underflow ex) {
+        std::cout << "Proper underflow error thrown" << std::endl;
+    }
+    
+    try {
+        testDeque->enqueue_head(33);
+        testDeque->enqueue_head(53);
+        testDeque->enqueue_tail(73);
+        testDeque->enqueue_tail(83);
+        testDeque->enqueue_tail(84);
+        testDeque->enqueue_tail(85);
+        testDeque->enqueue_tail(86);
+        testDeque->enqueue_tail(87);
+        testDeque->enqueue_tail(88);
+        testDeque->enqueue_tail(89);
+        testDeque->enqueue_tail(90);
+    } catch (overflow ex){
+        std::cout << "Proper underflow error thrown" << std::endl;
+    }
+    
     testDeque->dequeue_head();
     testDeque->dequeue_tail();
     
-    if(testDeque->size() == 2)
+    if(testDeque->size() == 8)
     {
         std::cout << "Proper size" << std::endl;
     }
@@ -45,7 +68,7 @@ int main() {
     {
         std::cout << "Error: Wrong head value" << std::endl;
     }
-    if(testDeque->tail() == 73)
+    if(testDeque->tail() == 88)
     {
         std::cout << "Proper tail value" << std::endl;
     }
